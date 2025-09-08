ef403e8fb101e46697db05f8474b331e9c4029a0
HerbertNtim <herbertntim15@gmail.com>
2025-09-08 17:36:53 +0000

    road borders

--- javascript/road.js
+++ javascript/road.js
@@ -15,7 +15,7 @@ class Road {
     const topRight = { x: this.right, y: this.top };
     const bottomLeft = { x: this.left, y: this.bottom };
     const bottomRight = { x: this.right, y: this.bottom };
-    this.borders = [[topLeft, bottomLeft][(topRight, bottomRight)]];
+    this.borders = [[topLeft, bottomLeft],[topRight, bottomRight]];
   }
 
   getLaneCenter(laneIndex) {
@@ -31,7 +31,7 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    for (let i = 0; i <= this.laneCount; i++) {
+    for (let i = 1; i <= this.laneCount - 1; i++) {
       const x = lerp(this.left, this.right, i / this.laneCount);
 
       ctx.setLineDash([20, 20]);
@@ -40,11 +40,12 @@ class Road {
       ctx.lineTo(x, this.bottom);
       ctx.stroke();
     }
+
     ctx.setLineDash([]);
     this.borders.forEach((border) => {
       ctx.beginPath();
       ctx.moveTo(border[0].x, border[0].y);
-      ctx.moveTo(border[1].x, border[1].y);
+      ctx.lineTo(border[1].x, border[1].y);
       ctx.stroke();
     });
   }
