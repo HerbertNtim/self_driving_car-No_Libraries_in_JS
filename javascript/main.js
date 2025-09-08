d3d45677108967524864bc1cd9e864cfea4f0073
HerbertNtim <herbertntim15@gmail.com>
2025-09-08 12:17:15 +0000

    defined road

--- javascript/main.js
+++ javascript/main.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9)
 const car = new Car(100, 100, 30, 50);
 
 console.log(car)
@@ -13,6 +14,7 @@ function animate() {
   car.update();
   canvas.height = window.innerHeight;
 
+  road.draw(ctx)
   car.draw(ctx);
 
   requestAnimationFrame(animate);
