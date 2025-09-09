16dd981a146d2d8c320a15d2cbc3572b2a18e81a
HerbertNtim <herbertntim15@gmail.com>
2025-09-09 17:24:16 +0000

    fix minor error on console

--- javascript/sensor.js
+++ javascript/sensor.js
@@ -13,12 +13,12 @@ class Sensor {
     this.#castRays();
     this.readings = [];
     for (let i = 0; i < this.rays.length; i++) {
-      this.readings.push(roadBorders);
+      this.readings.push(this.#getReading(this.rays[i], roadBorders));
     }
   }
 
   // Reading and intersection to the road borders using sensors
-  #getReadings(ray, roadBorders) {
+  #getReading(ray, roadBorders) {
     let touches = [];
 
     for (let i = 0; i < roadBorders.length; i++) {
@@ -64,12 +64,24 @@ class Sensor {
   }
 
   draw(ctx) {
-    for (let i = 0; i < this.rayCount; i++) {
+    for (let i = 0; i < this.rayCount; i++) { 
+      let end = this.rays[i][1];
+      if(this.readings[i]) {
+        end = this.readings[i]
+      }
+
       ctx.beginPath();
       ctx.lineWidth = 2;
       ctx.strokeStyle = "yellow";
       ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-      ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "black";
+      ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+      ctx.lineTo(end.x, end.y);
       ctx.stroke();
     }
   }
