f340dee0cd56afc511977f527f46439d01a29f83
HerbertNtim <herbertntim15@gmail.com>
2025-09-15 23:02:51 +0000

    implemented feed forward algorithm

--- javascript/network.js
+++ javascript/network.js
@@ -9,7 +9,7 @@ class Level {
       this.weights[i] = new Array(outputCount);
     }
 
-    Level.#randomize();
+    Level.#randomize(this);
   }
 
   static #randomize(level) {
@@ -22,5 +22,22 @@ class Level {
     for (let i = 0; i < level.biases.length; i++) {
       level.biases[i] = Math.random() * 2 - 1;
     }
+  }
+
+  static feedForward(givenInputs, level) {
+    for (let i = 0; i < level.inputs.length; i++) {
+      level.inputs[i] = givenInputs[i];
+    }
+
+    for (let i = 0; i < level.outputs.length; i++) {
+      let sum = 0;
+      for (let j = 0; j < level.inputs.length; j++) {
+        sum += level.inputs[j] * level.weights[j][i];
+      }
+
+      sum > level.biases[i] ? (level.outputs = 1) : (level.outputs = 0); 
+    }
+
+    return level.outputs
   }
 }
