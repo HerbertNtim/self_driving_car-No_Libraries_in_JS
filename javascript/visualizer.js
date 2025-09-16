2985069dab345aafd83c8d8d25845f3de89012d5
HerbertNtim <herbertntim15@gmail.com>
2025-09-16 22:43:26 +0000

    finished network visualizer

--- javascript/visualizer.js
+++ javascript/visualizer.js
@@ -6,10 +6,30 @@ class Visualizer {
     const width = ctx.canvas.width - margin * 2;
     const height = ctx.canvas.height - margin * 2;
 
-    Visualizer.drawLevel(ctx, network.levels[0], left, top, width, height);
+    const levelHeight = height / network.levels.length;
+    for (let i = network.levels.length - 1; i >= 0; i--) {
+      const levelTop =
+        top +
+        lerp(
+          height - levelHeight,
+          0,
+          network.levels.length == 1 ? 0.5 : i / (network.levels.length - 1)
+        );
+
+      ctx.setLineDash([7, 3])
+      Visualizer.drawLevel(
+        ctx,
+        network.levels[i],
+        left,
+        levelTop,
+        width,
+        levelHeight,
+        i == network.levels.length - 1 ? ["🠉", "🠈", "🠊", "🠋"] : []
+      );
+    }
   }
 
-  static drawLevel(ctx, level, top, left, width, height) {
+  static drawLevel(ctx, level, left, top, width, height, outputLabels) {
     const right = left + width;
     const bottom = top + height;
 
@@ -63,6 +83,18 @@ class Visualizer {
       ctx.setLineDash([3, 3]);
       ctx.stroke();
       ctx.setLineDash([]);
+
+      if (outputLabels[i]) {
+        ctx.beginPath();
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "white";
+        ctx.font = nodeRadius * 1.5 + "px Arial";
+        ctx.fillText(outputLabels[i], x, top + nodeRadius * 0.1);
+        ctx.lineWidth = 0.5;
+        ctx.strokeText(outputLabels[i], x, top + nodeRadius * 0.1);
+      }
     }
   }
 
