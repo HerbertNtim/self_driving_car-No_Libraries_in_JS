6561a7b3dc38e97d15838e068e946f0c9edbb69b
HerbertNtim <herbertntim15@gmail.com>
2025-09-19 16:51:12 +0000

    drawing car on html

--- javascript/car.js
+++ javascript/car.js
@@ -13,13 +13,11 @@ class Car {
     this.angle = 0;
     this.damage = false;
 
-    this.useBrain = controlType == 'AI'
+    this.useBrain = controlType == "AI";
 
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
-      this.brain = new NeuralNetwork(
-        [this.sensor.rayCount, 6, 4]
-      );
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
     this.controls = new Controls(controlType);
   }
@@ -35,14 +33,14 @@ class Car {
       const offsets = this.sensor.readings.map((s) =>
         s == null ? 0 : 1 - s.offset
       );
-      const outputs = NeuralNetwork.feedForward(offsets, this.brain)
-      console.log(outputs)
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+      console.log(outputs);
 
-      if(this.useBrain) {
-        this.controls.forward = outputs[0]
-        this.controls.left = outputs[1]
-        this.controls.right = outputs[2]
-        this.controls.reverse = outputs[3]
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
       }
     }
   }
@@ -129,13 +127,17 @@ class Car {
   }
 
   draw(ctx, color, drawSensor = false) {
-    this.damage ? (ctx.fillStyle = "gray") : (ctx.fillStyle = color);
-    ctx.beginPath();
-    ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
-    for (let i = 1; i < this.polygon.length; i++) {
-      ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
-    }
-    ctx.fill();
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+    ctx.drawImage(
+      this.img,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.restore();
 
     if (this.sensor && drawSensor) {
       this.sensor.draw(ctx);
