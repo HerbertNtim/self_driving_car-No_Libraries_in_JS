2ae9392de134131e5085ab47f7a1ffe5d1eeb795
HerbertNtim <herbertntim15@gmail.com>
2025-09-20 05:24:26 +0000

    fine tuning

--- javascript/car.js
+++ javascript/car.js
@@ -1,5 +1,5 @@
 class Car {
-  constructor(x, y, width, height, controlType, maxSpeed = 3) {
+  constructor(x, y, width, height, controlType, maxSpeed = 3, color = "blue") {
     this.y = y;
     this.x = x;
     this.width = width;
@@ -20,6 +20,24 @@ class Car {
       this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
     this.controls = new Controls(controlType);
+
+    this.img = new Image();
+    this.img.src = "car.png";
+
+    // Masking car color
+    this.mask = document.createElement("canvas");
+    this.mask.width = width;
+    this.mask.height = height;
+
+    const maskCtx = this.mask.getContext("2d");
+    this.img.onload = () => {
+      maskCtx.fillStyle = color;
+      maskCtx.rect(0, 0, this.width, this.height);
+      maskCtx.fill();
+
+      maskCtx.globalCompositeOperation = "destination-atop";
+      maskCtx.drawImage(this.img, 0, 0, this.width, this.height);
+    };
   }
 
   update(roadBorders, traffic) {
@@ -126,10 +144,25 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color, drawSensor = false) {
+  draw(ctx, drawSensor = false) {
+    if (this.sensor && drawSensor) {
+      this.sensor.draw(ctx);
+    }
+    
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(-this.angle);
+    if (!this.damage) {
+      ctx.drawImage(
+        this.mask,
+        -this.width / 2,
+        -this.height / 2,
+        this.width,
+        this.height
+      );
+
+      ctx.globalCompositeOperation = "multiply";
+    }
     ctx.drawImage(
       this.img,
       -this.width / 2,
@@ -138,9 +171,5 @@ class Car {
       this.height
     );
     ctx.restore();
-
-    if (this.sensor && drawSensor) {
-      this.sensor.draw(ctx);
-    }
   }
 }
